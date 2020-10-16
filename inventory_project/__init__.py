5d8518b72c2e140aeeffad4b061a472026358fbe
JensDiemer <git@jensdiemer.de>
2020-10-16 20:01:01 +0200

    print Django version, too

--- inventory_project/__init__.py
+++ inventory_project/__init__.py
@@ -4,9 +4,11 @@
 
 import sys
 
+from django import __version__ as django_version
+
 from inventory import __version__
 
 
-if __name__ == "inventory_project":
-    if "--version" not in sys.argv:
-        print(f"PyInventory v{__version__}")
+if __name__ == 'inventory_project':
+    if '--version' not in sys.argv:
+        print(f'PyInventory v{__version__} (Django v{django_version})')

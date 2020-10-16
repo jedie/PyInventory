20d673a0cf9ace5a7358a0e3cece663e7e8f05ef
JensDiemer <git@jensdiemer.de>
2020-10-16 20:20:18 +0200

    Basic project usage:
    * Setup Item and Location model
    * Locations can be nested
    * Use django-tagulous for item/location tags
    * Auto assign user to created item/location entries

--- inventory/models/__init__.py
+++ inventory/models/__init__.py
@@ -1,1 +1,2 @@
 from inventory.models.item import ItemModel  # noqa
+from inventory.models.location import LocationModel  # noqa
---
+++ inventory/models/location.py
@@ -0,0 +1,33 @@
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from inventory.models.base import BaseModel
+
+
+class LocationModel(BaseModel):
+    """
+    A Storage for items.
+    """
+    description = RichTextUploadingField(
+        config_name='LocationModel.description',
+        verbose_name=_('LocationModel.description.verbose_name'),
+        help_text=_('LocationModel.description.help_text')
+    )
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        verbose_name=_('LocationModel.parent.verbose_name'),
+        help_text=_('LocationModel.parent.help_text')
+    )
+
+    def __str__(self):
+        if self.parent_id is None:
+            return self.name
+        else:
+            return f'{self.name} -> {self.parent}'
+
+    class Meta:
+        verbose_name = _('LocationModel.verbose_name')
+        verbose_name_plural = _('LocationModel.verbose_name_plural')

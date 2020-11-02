77ac2ae8ff618b2f04cc300fe9d707d7c4e8981f
JensDiemer <git@jensdiemer.de>
2020-11-02 09:11:46 +0100

    change template context key: version_string -> inventory_version_string
    So it's more unique ;)

--- inventory/context_processors.py
+++ inventory/context_processors.py
@@ -2,4 +2,4 @@ from inventory import __version__
 
 
 def inventory_version_string(request):
-    return {"version_string": f"v{__version__}"}
+    return {'inventory_version_string': f'v{__version__}'}

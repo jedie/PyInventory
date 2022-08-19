0fe19c7ed8467d678ded6808dec425df60985092
JensDiemer <git@jensdiemer.de>
2022-08-19 09:20:28 +0200

    Release as v0.15.0

--- src/inventory/__init__.py
+++ src/inventory/__init__.py
@@ -1,7 +1,7 @@
 """
     created 14.20.2020 by Jens Diemer <opensource@jensdiemer.de>
-    :copyleft: 2020-2021 by the PyInventory team, see AUTHORS for more details.
+    :copyleft: 2020-2022 by the PyInventory team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
-__version__ = "0.14.0"
+__version__ = "0.15.0"

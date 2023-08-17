bae292a96d1d73a342227bac4a9b3e9074e4bc8d
JensDiemer <git@jensdiemer.de>
2023-08-17 18:29:15 +0200

    Update from project template

--- inventory/__init__.py
+++ inventory/__init__.py
@@ -1,7 +1,11 @@
 """
+    PyInventory
+    Web based management to catalog things including state and location etc. using Python/Django.
+
     created 14.20.2020 by Jens Diemer <opensource@jensdiemer.de>
-    :copyleft: 2020-2022 by the PyInventory team, see AUTHORS for more details.
+    :copyleft: 2020-2023 by the PyInventory team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
 __version__ = '0.19.0'
+__author__ = 'Jens Diemer <PyInventory@jensdiemer.de>'
